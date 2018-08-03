2e69d9de44d4bc6b09cdf97479f595821147a953
Sophia-Yuanyuan Deng <yydeng87@gmail.com>
2018-08-02 22:05:46 -0700

    Add files via upload

---
+++ 204.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count = 0;
+        if (n < 2) return count;
+        boolean[] notprime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            if (notprime[i] == false) {
+                count++;
+            }
+            for (int j = 2; j * i < n; j++) {
+                notprime[i * j] = true;
+            }
+        }
+        return count;
+    }
+}
