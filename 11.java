c402800e197b92dd2621034f71188b05727cdc91
Sophia-Yuanyuan Deng <yydeng87@gmail.com>
2018-08-11 03:45:09 -0700

    Add files via upload

---
+++ 11.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public int maxArea(int[] height) {
+        if (height == null || height.length < 2) return 0;
+        int res = 0;
+        int i = 0, j = height.length - 1;
+        while (i < j) {
+            res = Math.max(res, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j]) i++;
+            else j--;
+        }
+        return res;
+    }
+}
---
+++ 526.java
@@ -0,0 +1,20 @@
+class Solution {
+    int count = 0;
+    public int countArrangement(int N) {
+        helper(N, 1, new boolean[N + 1]);
+        return count;
+    }
+    private void helper(int N, int pos, boolean[] used) {
+        if (pos >  N) {
+            count++;
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (!used[i] &&  (i % pos == 0 || pos % i == 0)) {
+                used[i] = true;
+                helper(N, pos + 1, used);
+                used[i] = false;
+            }
+        }
+    }
+}
